97a5f02f1f058958a84a83183e27275286d6fd66
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-19 00:22:30 +0000

    limit player guess

--- guess-game.rb
+++ guess-game.rb
@@ -1,15 +1,31 @@
-puts "In this game you will be guessing a number from 1 to 10."
+puts "In this game you will be guessing a number from 0 to 9."
 puts "If your guess number matches the a secret number,\nYOU WON!"
 puts # print empty line
 
-secret_num = rand(0 .. 10)
+secret_num = rand(0 .. 9)
 guess = nil
-guess_count = 0;
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
-
-while guess != secret_num
-    puts "Enter your guessed number"
-    guess = gets.chomp.to_i
+while guess != secret_num and !out_of_guesses
+    # if guess count is less than guess limit then increment guess count
+    if guess_count < guess_limit
+        puts "Enter your guessed number"
+        guess = gets.chomp.to_i
+        guess_count += 1;
+    else 
+        # if guess count is greater than guess limit then set out_of_guesses to true
+        out_of_guesses = true
+        break
+    end
 end
 
-puts "YOU WON!"
+if out_of_guesses
+    puts "You lose!"
+    puts "You have reached the maximum number of guesses."
+    puts "The secret number was #{secret_num}"
+else
+    puts "Congratulations! You won!"
+    puts "You guessed the secret number #{secret_num} and you did it in #{guess_count} guesses."
+end
